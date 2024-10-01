213ca794d964722d5fedb5bfb52dddf927055d5c
WhyUcry <iemelvan67@gmail.com>
2024-10-01 21:07:04 +0300

    update

--- blognew-main/client/app.vue
+++ blognew-main/client/app.vue
@@ -1,4 +1,19 @@
 <template>
   <Navbar />
   <NuxtPage />
+  <Footer />
 </template>
+
+<style>
+  html, body, #__nuxt {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  main {
+    flex: 1 1 auto;
+  }
+  
+</style>
